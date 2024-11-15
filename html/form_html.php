5def378b417335592a3a048a864113f5349dd16f
AutiCodes <prive@auticodes.nl>
2024-11-15 12:23:13 +0100

    Initial commit

---
+++ html/form_html.php
@@ -0,0 +1,146 @@
+<?php
+	echo '<form action="' . esc_url($_SERVER['REQUEST_URI']) . '" method="post">';
+
+    echo '<h2>T.R.M.C. aanmeldingsformulier</h2>';
+
+    // Firstname
+	echo '<p>';
+	echo '<strong>Voornaam</strong> (vereist) <br/>';
+	echo '<input type="text" name="firstname" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Last name
+	echo '<p>';
+	echo '<strong>Achternaam</strong> (vereist) <br/>';
+	echo '<input type="text" name="lastname" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Address
+	echo '<p>';
+	echo '<strong>Adres</strong> (vereist) <br/>';
+	echo '<input type="text" name="address" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // City
+	echo '<p>';
+	echo '<strong>Woonplaats</strong> (vereist) <br/>';
+	echo '<input type="text" name="city" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Postcode
+	echo '<p>';
+	echo '<strong>Postcode</strong> (vereist) <br/>';
+	echo '<input type="text" name="postal_code" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Email
+	echo '<p>';
+	echo '<strong>Email</strong> (vereist) <br/>';
+	echo '<input type="text" name="email" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Phonenumber
+	echo '<p>';
+	echo '<strong>Telefoonnummer</strong> (vereist) <br/>';
+	echo '<input type="text" name="phone" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Birthdate
+	echo '<p>';
+	echo '<strong>Geboortedatum</strong> (DD-MM-YYYY) (vereist) <br/>';
+	echo '<input type="text" name="birthdate" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Nationality
+	echo '<p>';
+	echo '<strong>Nationaliteit</strong> (vereist) <br/>';
+	echo '<input type="text" name="nationality" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+    // Certificates
+    echo '<p><strong>';
+    echo 'Welke brevetten gehaald';
+    echo '</strong></p>';
+
+	echo '<input type="hidden" value=0 name="glider_brevet">';
+    echo '<input type="checkbox" id="glider_brevet" name="glider_brevet" value=1/>';
+    echo '<label for="glider_brevet" style="padding-left: 5px; color: black;"> Zweef brevet A</label><br>';
+
+	echo '<input type="hidden" value=0 name="motor_brevet">';
+    echo '<input type="checkbox" id="motor_brevet" name="motor_brevet" value=1/>';
+    echo '<label for="glider_brevet" style="padding-left: 5px; color: black;"> Motor brevet A</label><br>';
+
+	echo '<input type="hidden" value=0 name="heli_brevet">';
+    echo '<input type="checkbox" id="heli_brevet" name="heli_brevet" value=1/>';
+    echo '<label for="heli_brevet" style="padding-left: 5px; color: black;"> Heli brevet A</label><br>';
+
+	echo '<input type="hidden" value=0 name="drone_brevet_a1">';
+    echo '<input type="checkbox" id="drone_brevet_a1" name="drone_brevet_a1" value=1/>';
+    echo '<label for="drone_brevet_a1" style="padding-left: 5px;"> Drone EU A1</label><br>';
+
+	echo '<input type="hidden" value=0 name="drone_brevet_a3">';
+    echo '<input type="checkbox" id="drone_brevet_a3" name="drone_brevet_a3" value=1/>';
+    echo '<label for="drone_brevet_a3" style="padding-left: 5px; color: black;"> Drone EU A3</label><br>';
+
+	echo '<input type="hidden" value=0 name="drone_brevet_a2">';
+    echo '<input type="checkbox" id="drone_brevet_a2" name="drone_brevet_a2" value=1/>';
+    echo '<label for="drone_brevet_a2" style="padding-left: 5px; color: black;"> Drone EU A2</label><br>';
+
+    // RDW number
+	echo '<p>';
+	echo '<strong>RDW registratienummer</strong> <br/>';
+	echo '<input type="text" name="rdw_number" size="40" style="color: black;"/>';
+	echo '</p>';
+
+    // Other club select
+    echo '<label for="other_club"><strong>Lid van een andere modelvliegclub?</strong> (vereist)</label>';
+    echo '<select name="other_club" id="other_club">';
+    echo '<option selected>Selecteer een optie</option>';
+    echo '<option value=0>Nee</option>';
+    echo '<option value=1>Ja</option>';
+    echo '</select>';
+
+    // Cringe spacing
+    echo '<br>';
+
+    // Other club if yes select
+	echo '<p>';
+	echo '<strong>Bij ja, welke modelvliegclub?</strong> <br/>';
+	echo '<input type="text" name="other_club_text" size="40" style="color: black;/>';
+	echo '</p>';
+
+    // KNVvl select
+    echo '<label for="knvvl_select"><strong>K.N.V.v.l.</strong> (vereist)</label>';
+    echo '<select name="knvvl_select" id="knvvl_select">';
+    echo '<option selected>Selecteer een optie</option>';
+    echo '<option value=0>Nee</option>';
+    echo '<option value=1>Ja</option>';
+    echo '</select>';
+
+    // Cringe spacing
+    echo '<br>';
+
+    // If KNVvl is member, number
+	echo '<p>';
+	echo '<strong>Bij ja, K.N.V.v.l. registratienummer</strong> <br/>';
+	echo '<input type="text" name="knvvl_text" size="40" style="color: black;"/>';
+	echo '</p>';
+
+    // Wanne be member of TRMC at:
+	echo '<p>';
+	echo '<strong>Ik wil lid worden van de T.R.M.C. per</strong> (DD-MM-YYYY) (vereist) <br/>';
+	echo '<input type="text" name="wanna_be_member_at" size="40" required style="color: black;"/>';
+	echo '</p>';
+
+	// Anti bot
+	echo '<p>';
+	echo '<strong>Anti bot vraag</strong> <br/>';
+	echo '<input type="text" name="anti_bot" size="40" required" style="color: black;" placeholder="Wat is 3 + 3?"/>';
+
+	// Cringe spacing
+	echo '<br>';
+
+	echo '<p><input type="submit" name="cf-submitted" value="Verstuur"></p>';
+
+	echo '</form>';
+
---
+++ wordpress-to-kiyomi.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: WordPress naar kiyomi
+Plugin URI: https://github.com/AutiCodes/WordPress-to-kiyomi
+Description: Deze plugin stuurt nieuwe leden aanmeldingen naar kiyomi over een API
+Version: 1.0
+Author: Kelvin de Reus AKA AutiCodes
+Author URI: https://auticodes.nl
+*/
+
+/**
+ * Handles the form submitted
+ * 
+ * @author AutiCodes
+ * 
+ * @return void
+ */
+function handleNewMemberSubmit(): void
+{
+    if (isset($_POST['cf-submitted'])) {
+        if (sanitize_text_field($_POST['anti_bot']) != 6) {
+            echo 'Er ging iets mis! Bot vraag is verkeerd!';
+            return;
+        }
+
+        $data = array(
+            'name' => sanitize_text_field($_POST['firstname']) . ' ' . sanitize_text_field($_POST['lastname']),
+            'first_name' => sanitize_text_field($_POST['firstname']),
+            'address' => sanitize_text_field($_POST['address']),
+            'city' => sanitize_text_field($_POST['city']),
+            'postcode' => sanitize_text_field($_POST['postal_code']),
+            'email' => sanitize_text_field($_POST['email']),
+            'phone' => sanitize_text_field($_POST['phone']),
+            'birthdate' => date('Y-m-d', strtotime(sanitize_text_field($_POST['birthdate']))),
+            'nationality' => sanitize_text_field($_POST['nationality']),
+            'has_glider_brevet' => sanitize_text_field($_POST['glider_brevet']) ?? 0,
+            'has_plane_brevet' => sanitize_text_field($_POST['motor_brevet']) ?? 0,
+            'has_helicopter_brevet' => sanitize_text_field($_POST['heli_brevet']) ?? 0,
+            'has_drone_a1' => sanitize_text_field($_POST['drone_brevet_a1']) ?? 0,
+            'has_drone_a2' => sanitize_text_field($_POST['drone_brevet_a2']) ?? 0,
+            'has_drone_a3' => sanitize_text_field($_POST['drone_brevet_a3']) ?? 0,
+            'rdw_number' => sanitize_text_field($_POST['rdw_number']) ?? 0,
+            'is_member_of_other_club' => sanitize_text_field($_POST['other_club_text']) ?? 0,
+            'KNVvl' => sanitize_text_field($_POST['knvvl_text']),
+            'wanna_be_member_at' => date('Y-m-d', strtotime(sanitize_text_field($_POST['wanna_be_member_at']))),
+            'api_key' => KIYOMI_API_KEY,
+        );
+
+        $curl = curl_init(KIYOMI_API_URL);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen(json_encode($data))
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        echo $response;
+
+        // Show succes html text from external file
+		include('html/succes_form_html.php');
+    }
+}
+
+/**
+ * Function to register the shortcode
+ *
+ * @author AutiCodes
+ *
+ * @return object ob_get_clean()
+ */
+function regShortcode()
+{
+	ob_start();
+	handleNewMemberSubmit();
+	include('html/form_html.php');
+
+	return ob_get_clean();
+}
+
+// Registers the shortcode
+add_shortcode('trmc_new_member_cf', 'regShortcode');
